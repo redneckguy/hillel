5f3818ec6b196f7b87c54b72cce622ba3e993de8
Dimitri <dmityvo83@gmail.com>
2021-05-30 17:56:16 +0300

    Jonas JS Challenge #1

--- jonas JS/assignments/script.js
+++ jonas JS/assignments/script.js
@@ -1,7 +1,7 @@
 // DZ 1
 const country = "Ukraine";
 const continent = "Europe";
-let population = "42 million";
+let population = 42000000;
 
 // console.log(country, continent, population);
 
@@ -17,12 +17,65 @@ let language;
 //DZ 3
 language = "Ukrainian";
 
-console.log(country, continent, population);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof isIsland);
-console.log(typeof language);
+// console.log(country, continent, population);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof isIsland);
+// console.log(typeof language);
+
+//DZ 4
+let halfPopulation = population / 2;
+console.log(halfPopulation);
+
+halfPopulation++;
+console.log(halfPopulation);
+
+let finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+
+let averagePopulation = 33000000;
+console.log(population < averagePopulation);
+
+let description =
+    country +
+    " is in " +
+    continent +
+    ", and its " +
+    population +
+    " people speak " +
+    language;
+console.log(description);
+
+console.log("         ");
 
 
+// * ===== CHALLANGE #1 =====
 
+console.log("Challange #1");
+
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark = massMark / heightMark ** 2;
+let BMIJohn = massJohn / heightJohn ** 2;
+
+let markHighterBMI = BMIMark > BMIJohn;
+
+console.log(markHighterBMI);
+
+massMark = 95;
+heightMark = 1.88;
+
+massJohn = 85;
+heightJohn = 1.76;
+
+BMIMark = massMark / heightMark ** 2;
+BMIJohn = massJohn / heightJohn ** 2;
+
+markHighterBMI = BMIMark > BMIJohn;
+
+console.log(markHighterBMI);
 
